baf2f93e4372770592e93be5a2e51035a383e359
tslight <tslight@pm.com>
2020-03-20 01:02:17 +0000

    removed lib from gitignore

---
+++ azure_extras/lib/az.py
@@ -0,0 +1,123 @@
+import json
+import logging
+import re
+import requests
+import sys
+import traceback
+from configparser import ConfigParser
+from azure.common.credentials import ServicePrincipalCredentials
+from bs4 import BeautifulSoup
+
+
+def get_access_token(client_id, secret, tenant):
+    try:
+        credentials = ServicePrincipalCredentials(
+            client_id=client_id, secret=secret, tenant=tenant,
+        )
+        access_token = credentials.token["access_token"]
+        logging.debug(f"Access Token:\n\n{access_token}\n")
+        return access_token
+    except Exception as error:
+        logging.debug(traceback.format_exc())
+        raise error
+
+
+def get_az_details(path):
+    try:
+        config = ConfigParser()
+        config.read(path)
+        az_details = config["azure"]
+    except Exception as error:
+        logging.error(f"Failed to retrieve config from {path}. Aborting.")
+        logging.error(error)
+        sys.exit(1)
+
+    if az_details:
+        logging.info(f"Found Azure configuration at {path}.")
+        return az_details
+
+    logging.error(f"Failed to retrieve Azure details from {path}. Aborting.")
+    sys.exit(1)
+
+
+class AzureExtras:
+    def __init__(self, config_path):
+        self.az = get_az_details(config_path)
+        token = get_access_token(
+            self.az["client_id"], self.az["secret"], self.az["tenant"]
+        )
+        self.headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        }
+
+    def enable_health_check(self, rg, app):
+        url = (
+            f"https://management.azure.com/subscriptions/{self.az['sub_id']}/"
+            + f"resourceGroups/{rg}/providers/Microsoft.Web/sites/{app}/config/web"
+        )
+        params = {"api-version": "2018-02-01"}
+        patch = {"properties": {"healthCheckPath": "/status/status.cshtml"}}
+
+        try:
+            response = requests.patch(
+                url, headers=self.headers, params=params, data=json.dumps(patch)
+            )
+            content = response.json()
+            success = (
+                content["properties"]["healthCheckPath"]
+                == patch["properties"]["healthCheckPath"]
+            )
+            logging.debug(json.dumps(content, indent=2, sort_keys=True))
+            if response.ok is False or success is False:
+                raise AssertionError(
+                    f"Failed to enable App Service Health Check on {app}\n"
+                    + f"Code:{response.status_code}"
+                )
+        except Exception as error:
+            raise error
+
+    def get_publish_profile(self, rg, app):
+        """
+        Get FTP and Web App credentials (username, password, hostname)
+        https://docs.microsoft.com/en-us/rest/api/appservice/webapps/listpublishingprofilexmlwithsecrets
+        """
+        url = (
+            f"https://management.azure.com/subscriptions/{self.az['sub_id']}/"
+            + f"resourceGroups/{rg}/providers/Microsoft.Web/sites/{app}/publishxml"
+        )
+        params = {"api-version": "2016-08-01"}
+
+        try:
+            response = requests.post(url, headers=self.headers, params=params)
+
+            if response.ok:
+                soup = BeautifulSoup(response.text, features="html.parser")
+                logging.debug(f"Response XML:\n\n{soup.prettify()}\n")
+
+                web_pp = soup.select("publishProfile")[0]
+                ftp_pp = soup.select("publishProfile")[1]
+                ftp_host = re.search(
+                    r"ftp:\/\/(.*)/site/wwwroot", ftp_pp["publishurl"], re.IGNORECASE
+                ).group(1)
+
+                publish_profile = {
+                    "web_url": web_pp["destinationappurl"],
+                    "web_user": web_pp["username"],
+                    "web_passwd": web_pp["userpwd"],
+                    "ftp_host": ftp_host,
+                    "ftp_user": ftp_pp["username"],
+                    "ftp_passwd": ftp_pp["userpwd"],
+                }
+
+                logging.debug(
+                    "Publish Profile:\n" + json.dumps(publish_profile, indent=2)
+                )
+                return publish_profile
+
+            logging.debug(f"Subscription ID: {sub}")
+            logging.debug("\n" + json.dumps(response.json(), indent=2, sort_keys=True))
+            raise AssertionError(f"{response.json()['error']['message']}")
+        except Exception as error:
+            logging.debug(traceback.format_exc())
+            raise error
---
+++ azure_extras/lib/kudu.py
@@ -0,0 +1,128 @@
+import json
+import logging
+import requests
+import traceback
+from .az import AzureExtras
+from time import time
+
+
+class KuduClient:
+    """
+    https://github.com/projectkudu/kudu/wiki/REST-API
+    """
+
+    def __init__(self, config_path, rg, app):
+        az = AzureExtras(config_path)
+        self.pp = az.get_publish_profile(rg, app)
+        self.url = (
+            self.pp["web_url"]
+            .replace("http://", "https://")
+            .replace("azurewebsites", "scm.azurewebsites")
+            + "/api/"
+        )
+        self.auth = (self.pp["web_user"], self.pp["web_passwd"])
+        logging.debug(f"URL: {self.pp['web_url']}")
+        logging.debug(f"KUDU URL: {self.url}")
+        logging.debug(f"USER: {self.pp['web_user']}, PASS: {self.pp['web_passwd']}")
+
+    def deploy_zip(self, path):
+        """
+        https://github.com/projectkudu/kudu/wiki/REST-API#zip-deployment
+
+        Deploy from zip asynchronously. The Location header of the response
+        will contain a link to a pollable deployment status.
+        """
+        try:
+            with open(path, "rb") as f:
+                zipfile = f.read()
+
+            response = requests.put(
+                self.url + "zipdeploy?isAsync=true", auth=self.auth, data=zipfile
+            )
+
+            if response.ok:
+                timeout = time() + 60
+                while time() < timeout:
+                    logging.debug(f"Checking deployment {response.headers['Location']}")
+                    status = requests.get(response.headers["Location"], auth=self.auth)
+                    if status.json()["complete"]:
+                        return status.json()
+
+            raise AssertionError(
+                f"Deploying {path} on {self.url}zipdeploy\n"
+                + f"Code:{response.status_code}, Message: {response.text}"
+            )
+        except Exception as error:
+            logging.debug(f"Failed to deploy {path} from {self.url}zipdeploy: {error}")
+            logging.debug(traceback.format_exc())
+            raise error
+
+    def download_zip(self, source, destination):
+        """
+        https://github.com/projectkudu/kudu/wiki/REST-API#zip
+
+        Zip up and download the specified folder. The zip doesn't include the
+        top folder itself. Make sure you include the trailing slash!
+        """
+        try:
+            response = requests.get(f"{self.url}zip/{source}", auth=self.auth)
+            logging.debug(response.headers)
+            zipbytes = response.content
+            zipfile = open(destination, "wb")
+            zipfile.write(zipbytes)
+            zipfile.close()
+        except Exception as error:
+            logging.debug(
+                f"Failed to download zip from {self.url}zip/{source}: {error}"
+            )
+            logging.debug(traceback.format_exc())
+            raise error
+
+        return response
+
+    def get_endpoint(self, endpoint):
+        """
+        https://github.com/projectkudu/kudu/wiki/REST-API
+        """
+        try:
+            response = requests.get(self.url + endpoint, auth=self.auth)
+            if response.ok is False:
+                raise AssertionError(
+                    f"Failed to get {endpoint} on {self.url}\n"
+                    + f"Code:{response.status_code}\n"
+                    + f"Response: {response.text}"
+                )
+        except Exception as error:
+            logging.debug(f"Failed to get {endpoint} from {self.url}: {error}")
+            logging.debug(traceback.format_exc())
+            raise error
+
+        logging.debug(
+            "Output:\n" + json.dumps(response.json(), indent=2, sort_keys=True)
+        )
+        return response.json()
+
+    def run_cmd(self, cmd, cwd):
+        """
+        https://github.com/projectkudu/kudu/wiki/REST-API#command
+        """
+        logging.debug(f"Running {cmd} on {self.pp['web_url']}/{cwd}...")
+        payload = {"command": cmd, "dir": cwd}
+
+        try:
+            response = requests.post(self.url + "command", auth=self.auth, json=payload)
+            if response.ok is False:
+                raise AssertionError(
+                    f"Failed to run {cmd} on {self.pp['web_url']}/{cwd}\n"
+                    + f"Code:{response.status_code}\n"
+                    + f"Response: {response.text}"
+                )
+        except Exception as error:
+            logging.debug(f"Failed to run {cmd} on {self.pp['web_url']}/{cwd}: {error}")
+            logging.debug(traceback.format_exc())
+            raise error
+
+        logging.debug(
+            "Output:\n" + json.dumps(response.json(), indent=2, sort_keys=True)
+        )
+        return response.json()
