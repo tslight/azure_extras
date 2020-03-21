d66a58715647c62faf0e1283cbbf453db9ce3310
tslight <tslight@pm.com>
2020-03-21 23:14:09 +0000

    export more stuff from the module

--- azure_extras/__init__.py
+++ azure_extras/__init__.py
@@ -3,4 +3,12 @@
 
 name = "azure_extras"
 from .lib.az import AzureExtras
+from .lib.app import AppService
 from .lib.kudu import KuduClient
+from .lib.saj import StreamAnalyticsJobs
+from .asctl import asctl
+from .healthchkctl import healthchkctl
+from .kudu import run_cmd
+from .kudu import get_endpoint
+from .kudu import download_zip
+from .kudu import deploy_zip
