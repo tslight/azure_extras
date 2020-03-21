3e238bd98b88462c34a63cb62321236773bc2052
tslight <tslight@pm.com>
2020-03-21 23:14:38 +0000

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="azure_extras",
-    version="0.1.5",
+    version="0.1.6",
     author="Toby Slight",
     author_email="tslight@pm.me",
     description="The stuff Microsoft left out..",
