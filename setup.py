3377c1ea4183586851a88c5c249a380fc69384dd
tslight <tslight@pm.com>
2021-12-14 22:08:54 +0000

    try with hardcoding version

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="azure_extras",
-    version=get_latest_tag(),
+    # version=get_latest_tag(),
+    version="0.2.0",
     author="Toby Slight",
     author_email="tslight@pm.me",
     description="The stuff Microsoft left out..",
